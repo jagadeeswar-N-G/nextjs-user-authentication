9b7305cfe99b1fdf849bd6ff413f6f162bdbed31
jagadeeswar-N-G <jagadeeswar8055@gmail.com>
2023-11-02 20:15:51 +0530

    first commit

---
+++ src/app/dbConfig/dbCOnfig.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import React from "react";
+
+const connect = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("connection is successful");
+    });
+    connection.on("error", (err) => {
+      console.log("mongo connectoin error" + err);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("something went wrong");
+    console.log(error);
+  }
+};
+
+export default connect;
---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfileUser = ({params}: any) => {
+  return (
+    <div>ProfileUser {params.id}</div>
+  )
+}
+
+export default ProfileUser
