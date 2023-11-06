e58293a96b0441b0d58f8746199adfb3019fbda0
jagadeeswar-N-G <jagadeeswar8055@gmail.com>
2023-11-06 10:50:18 +0530

    added the component

--- src/app/api/users/myself/route.ts
+++ src/app/api/users/myself/route.ts
@@ -3,17 +3,24 @@ import User from "@/models/userModels";
 import { NextRequest, NextResponse } from "next/server";
 import { connect } from "@/app/dbConfig/dbConfig";
 
-connect()
+connect();
 
-export const GET = async(request: NextRequest) => {
-    try {
-        const userId = await getDatafromToken(request);
-        const user = await User.findOne({_id: userId}).select("-password");
-        return NextResponse.json({
-            message: "user found",
-            data: user
-        })
-    } catch (error) {
-        
-    }
-}
+export const GET = async (request: NextRequest) => {
+  try {
+    const userId = await getDatafromToken(request);
+    const user = await User.findOne({ _id: userId }).select("-password");
+    return NextResponse.json({
+      message: "user found",
+      data: user,
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        error: error.message,
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+};
